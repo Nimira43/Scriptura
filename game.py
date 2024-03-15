2675cdb325038bc7b1179f46990d7af67718c7d6
Nimira43 <n.rabone72@aol.com>
2024-03-15 08:42:23 +0000

    Updated foundational structure

--- game.py
+++ game.py
@@ -1,23 +1,25 @@
 import pygame
+from character import Character
 import gamesettings as gs
 
 class Game:
     def __init__(self, main, assets):
         self.MAIN = main
         self.ASSETS = assets
+        self.player = Character(self)
     
     def input(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.MAIN.run = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.MAIN.run = False
+        #for event in pygame.event.get():
+        #    if event.type == pygame.QUIT:
+        #        self.MAIN.run = False
+        #    elif event.type == pygame.KEYDOWN:
+        #        if event.key == pygame.K_ESCAPE:
+        #            self.MAIN.run = False
+        self.player.input()
     
     def update(self):
-        pass
+        self.player.update()
 
     def draw(self, window):
-        pass
+        self.player.draw(window)
 
-    
