92d5cc065c49327e5ba3bcb4d708677d504ad97a
Nimira43 <n.rabone72@aol.com>
2025-10-30 04:10:39 +0000

    Assigned timeoutId to the setTimeout

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ const input = document.querySelector('input')
 let timeoutId
 
 const onInput = event => {
-  setTimeout(() => {
+  timeoutId = setTimeout(() => {
     fetchData(event.target.value)
   }, 1000)
 }
